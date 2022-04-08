999f7510c9f355d59225f5c3bb8b906c2f0df7d8
Otávio Santos <otavio.santos@el.com.br>
2022-04-08 11:48:36 -0300

    Adicionando projeto

---
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -0,0 +1,87 @@
+package br.com.caelum.camel;
+
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http4.HttpMethods;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.http.entity.ContentType;
+
+public class RotaPedidos {
+
+	public static void main(String[] args) throws Exception {
+
+		CamelContext context = new DefaultCamelContext();
+		context.addComponent("activemq",
+				ActiveMQComponent.activeMQComponent("tcp://localhost:61615"));
+
+		// DSL = Domain Specific Language
+
+		context.addRoutes(new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+
+				errorHandler(
+//						deadLetterChannel("file:erro")
+						deadLetterChannel("activemq:queue:pedidos.DLQ")
+//							.logExhaustedMessageHistory(true)
+//							.useOriginalMessage()
+							.maximumRedeliveries(3)
+							.redeliveryDelay(2000)
+							.onRedelivery(new Processor() {
+								@Override
+								public void process(Exchange exchange) throws Exception {
+									int counter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
+									int max = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
+									System.out.println("Redelivery " + counter + "/" + max);
+								}
+							})
+				);
+
+//				from("file://pedidos?delay=5s&noop=true")
+				from("activemq:queue:pedidos")
+						.routeId("rota-pedidos")
+						.delay(1000)
+						.to("validator:pedido.xsd")
+						.multicast()
+//						.parallelProcessing()
+//						.timeout(500)
+							.to("direct:http")
+							.to("direct:soap");
+
+				from("direct:http")
+						.routeId("rota-http")
+						.setProperty("pedidoId", xpath("/pedido/id/text()"))
+						.setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()"))
+						.split()
+							.xpath("/pedido/itens/item")
+						.filter()
+							.xpath("/item/formato[text()]='EBOOK'")
+						.setProperty("ebookId", xpath("/item/livro/codigo/text()"))
+						.marshal()
+						.xmljson()
+						.log("${id} - ${body}")
+//						.setHeader(Exchange.FILE_NAME, simple("${file:name.noext}-${header.CamelSplitIndex}.json"))
+//						.to("file:saldo");
+						.setHeader(Exchange.HTTP_METHOD, HttpMethods.GET)
+//						.setHeader(Exchange.HTTP_QUERY,
+//								simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}"))
+						.to("http4://localhost:8080/webservices/ebook/item");
+
+				from("direct:soap")
+						.routeId("rota-soap")
+						.to("xslt:pedido-para-soap.xslt")
+//						.to("mock:soap");
+						.setHeader(Exchange.CONTENT_TYPE, constant("text/xml"))
+						.to("http4://localhost:8080/webservices/financeiro");
+
+			}
+		});
+
+		context.start();
+		Thread.sleep(1000 * 1000);
+		context.stop();
+	}
+}
